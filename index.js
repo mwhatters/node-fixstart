ce3b027a6912b556e5625ccf28452b04033a0a56
mwhatters <mwhatters@gmail.com>
2015-08-10 12:47:20 -0700

    fix-created emitter

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ var url = require('url');
 var redis = require('redis');
 
 var issueClient = redis.createClient();
-issueClient.subscribe('issue-created')
+var fixClient = redis.createClient();
 
+issueClient.subscribe('issue-created')
+fixClient.subscribe('fix-created')
 
 io.on('connection', function(socket) {
 });
 
 issueClient.on('message', function(channel, data) {
 	console.log('issue-created message recieved')
-	console.log(data)
 	io.sockets.emit('issue-created', JSON.parse(data))
+})
+
+
+fixClient.on('message', function(channel, data){
+	console.log('fix-created message recieved')
+	console.log(data)
+	io.sockets.emit('fix-created', JSON.parse(data))
 })
