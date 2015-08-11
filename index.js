d6a943a30a344f81a6cc7deb8f8f6918511422b1
mwhatters <mwhatters@gmail.com>
2015-08-10 18:03:17 -0700

    heroku env setup

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
+require('dotenv').load();
 var io = require('socket.io').listen(parseInt(process.env.PORT) || 5001);
 var url = require('url');
 var redis = require('redis');
 
-var issueClient = redis.createClient();
-var fixClient = redis.createClient();
-var localStreamClient = redis.createClient();
+
+// HEROKU SETUP
+if (process.env.REDISCLOUD_URL) {
+    var redisURL = url.parse(process.env.REDISCLOUD_URL);
+    var issueClient = redis.createClient(redisURL.port, redisURL.hostname, {
+        no_ready_check: true
+    });
+    var fixClient = redis.createClient(redisURL.port, redisURL.hostname, {
+        no_ready_check: true
+    });
+    var localStreamClient = redis.createClient(redisURL.port, redisURL.hostname, {
+        no_ready_check: true
+    });
+
+    issueClient.auth(redisURL.auth.split(":")[1]);
+    fixClient.auth(redisURL.auth.split(":")[1]);
+    localStreamClient.auth(redisURL.auth.split(":")[1]);
+
+} else {
+    var issueClient = redis.createClient();
+    var fixClient = redis.createClient();
+    var localStreamClient = redis.createClient();
+}
 
 issueClient.subscribe('issue-created')
 fixClient.subscribe('fix-created')
 localStreamClient.subscribe('stream')
 
 
-io.on('connection', function(socket) {
-});
+io.on('connection', function(socket) {});
 
 issueClient.on('message', function(channel, data) {
-	console.log('issue-created message recieved')
-	console.log(data)
-	io.sockets.emit('issue-created', JSON.parse(data))
+    console.log('issue-created message recieved')
+    console.log(data)
+    io.sockets.emit('issue-created', JSON.parse(data))
 })
 
 
-fixClient.on('message', function(channel, data){
-	console.log('fix-created message recieved')
-	console.log(data)
-	io.sockets.emit('fix-created', JSON.parse(data))
+fixClient.on('message', function(channel, data) {
+    console.log('fix-created message recieved')
+    console.log(data)
+    io.sockets.emit('fix-created', JSON.parse(data))
 })
 
-localStreamClient.on('message', function(channel, data){
-	console.log('stream message recieved')
-	console.log(data)
-	io.sockets.emit('stream', JSON.parse(data))
+localStreamClient.on('message', function(channel, data) {
+    console.log('stream message recieved')
+    console.log(data)
+    io.sockets.emit('stream', JSON.parse(data))
 })
-
-
-
-// heroku
-// var redisURL = url.parse(process.env.REDISCLOUD_URL);
-// var commentClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// var roomClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// commentClient.auth(redisURL.auth.split(":")[1]);
-// roomClient.auth(redisURL.auth.split(":")[1]);
